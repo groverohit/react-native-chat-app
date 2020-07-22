685a356e79dbf801e3db5697cdd30d283f2e2d59
Rohit Grover <groverohit@gmail.com>
2020-07-22 14:48:54 -0700

    Integrate Gifted Chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,46 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+  state = {
+    messages: [],
+  };
+
+  componentDidMount() {
+    this.setState({
+      //default messages to load
+      messages: [
+        {
+          _id: 1,
+          text: "Hello " + this.props.route.params.name,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "Normal message",
+          createdAt: new Date(),
+          user: {
+            _id: 3,
+            name: "React Native",
+            avatar: "https://placeimg.com/141/141/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     //store the background color to use
     let color = this.props.route.params.color;
@@ -10,19 +49,16 @@ export default class Chat extends React.Component {
     let name = this.props.route.params.name;
 
     //sets the title
-    this.props.navigation.setOptions({ title: name });
+    //this.props.navigation.setOptions({ title: name });
 
     return (
-      <View
-        style={{
-          backgroundColor: color,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
         }}
-      >
-        <Text style={{ color: "red" }}>This is the Chat Screen</Text>
-      </View>
+      />
     );
   }
 }
